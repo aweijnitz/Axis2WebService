55a0dbf5724eb376168736e57ca77d2706eada5a
Anders Weijnitz <anders.weijnitz@gmail.com>
2015-07-26 22:41:36 +0200

    Working version

---
+++ src/info/andersw/ws/MyService.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.andersw.ws;
+
+/**
+ *
+ * @author anders
+ */
+public class MyService {
+
+    public String getMessage(String input) {
+        return "Hej " + input + "!";
+    }
+}
